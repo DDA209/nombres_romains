aadb0873153046403061b5bd88873104c41ddd53
DDA209 <didier.jeux1@gmail.com>
2020-08-08 20:02:08 +0200

    final commit

--- nombre_romain.js
+++ nombre_romain.js
@@ -1,73 +1,100 @@
 function parseToRoman(num) {
-    let equivalences = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
 
-    let result = 'Supérieur à 9';
+    let result = '';
 
-    const arrNum = num.toString().split('');
-    const numLength = arrNum.length;
-
-    console.log ('chiffre', arrNum);
-    console.log ('longueur', numLength);
-    
-    if (numLength === 1) {
-        let arrLogic = getLogical(num);
-        let pos0 = equivalences[numLength].repeat(arrLogic[0]);
-        let pos1 = equivalences[numLength - 1].repeat(arrLogic[1]);
-        let pos2 = equivalences[numLength].repeat(arrLogic[2]);
-        let pos3 = equivalences[numLength + 1].repeat(arrLogic[3]);
-
-        let arrResult =  [pos0 ,pos1, pos2, pos3];
-        console.log('arrResult', arrResult);
-        result = arrResult.join('');
+    if (num === 0) {
+        return 'Zéro';
     }
 
-    // console.log (numLength);
+    let equivalences = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
+
+    const arrStrNum = num.toString().split('').reverse();
+    const arrNum = arrStrNum.map( e => parseInt(e));
+    const numLength = arrNum.length;
+
+    // console.log ('Array', arrStrNum, '-> chiffre', arrNum, '/ longueur', numLength);
+    
+    for (let i = 0; i < numLength; i++) {
+        let cond = true;
+        let digit = arrNum[i];
+        let j = i;
+        // console.log('parseToRoman arrNum[i]', arrNum[i], 'index', i);
+
+        if ( i >= 3 ){
+            cond = false;
+            j = 3;
+        }
+
+        // console.log(num, '< 1000 :', cond, "- i =", i, "et j =", j);
+        // console.log('Math.pow(10, j - i) =', Math.pow(10, i - j));
+
+        let arrLogic = getLogical(digit * (Math.pow(10, i - j)), cond );
+        let pos0 = cond ? equivalences[ 2 * j + 1].repeat(arrLogic[0]) : '';
+        let pos1 = equivalences[ 2 * j ].repeat(arrLogic[1]);
+        let pos2 = cond ? equivalences[ 2 * j + 1].repeat(arrLogic[2]) : '';
+        let pos3 = cond ? equivalences[ 2 * j + 2 ].repeat(arrLogic[3]) : '';
+
+        let arrResult =  [pos0 ,pos1, pos2, pos3];
+        // console.log('arrResult', arrResult);
+        result = arrResult.join('') + result;
+
+    }
 
     return result;
 }
 
+function getLogical(digit, cond) {
 
-function getLogical(digit) {
     result = [0, 0, 0, 0];
   
     if (digit < 4){
         result = [0, digit, 0, 0];
     };
 
-    if (digit === 4){
-        result = [0, digit - 3, 1, 0];
+    if (digit === 4 && cond){
+        result = [0, 1, 1, 0];
     };
 
-    if (digit > 4 && digit < 9){
+    if (digit > 4 && digit < 9 && cond){
         result = [1, digit - 5, 0, 0];
-    };
+    }
+    
+    if (digit === 9  && cond){
+        result = [0, 1, 0, 1];
+    }
 
-    if (digit === 9){
-        result = [0, digit - 8, 0, 1];
-    };
+    if (digit >= 4 && !cond) {
+        result = [0, digit , 0, 0];
+    }
 
-    console.log('getLogical', result);
+
+    // console.log('getLogical type digit', typeof digit, '- getLogical', result);
     return result
 }
 
-let num1 = 4;
-let num2 = 37;
-let num3 = 143;
-let num4 = 1234;
-let num5 = 6;
-let num6 = 3;
-let num7 = 9;
-let num8 = 1;
-let num9 = 0;
-let num10 = 10;
 
-console.log (num1, ' = ', parseToRoman(num1));
-console.log (num2, ' = ', parseToRoman(num2));
-console.log (num3, ' = ', parseToRoman(num3));
-console.log (num4, ' = ', parseToRoman(num4));
-console.log (num5, ' = ', parseToRoman(num5));
-console.log (num6, ' = ', parseToRoman(num6));
-console.log (num7, ' = ', parseToRoman(num7));
-console.log (num8, ' = ', parseToRoman(num8));
-console.log (num9, ' = ', parseToRoman(num9));
-console.log (num10, ' = ', parseToRoman(num10));
+
+let nums = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    0,
+    37,
+    143,
+    1234,
+    2020,
+    9999,
+    10001,
+    44444,
+]
+
+nums.forEach(element => {
+    console.log (element, ' = ', parseToRoman(element));    
+});
